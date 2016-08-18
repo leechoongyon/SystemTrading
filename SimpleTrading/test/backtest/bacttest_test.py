d634fbce02e45bfcf397c5259346612ae30d81c9
lee <lee>
2016-08-18 16:13:35 +0900

    a

--- SimpleTrading/test/backtest/bacttest_test.py
+++ SimpleTrading/test/backtest/bacttest_test.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 '''
 Created on 2016. 8. 17.
 
@@ -6,6 +7,7 @@ Created on 2016. 8. 17.
 
 import datetime
 import time
+import matplotlib.pyplot as plt
 
 from zipline.algorithm import TradingAlgorithm
 from zipline.api import order, record, symbol
@@ -16,20 +18,22 @@ from simple.common.util.time_util import getDayFromSpecificDay, \
     getTodayWithFormatting
 from simple.data.controlway.crawler.data_crawler import getHistoricalData
 
-
+ 
 def initialize(context):
     pass
 
-    
+     
 def handle_data(context, data):
     order(symbol('035720'), 1)
 
 if __name__ == '__main__':
     
     stockCd = "035720"
+    path = "C:/Windows/System32/git/PythonExample/PythonTetEnv/stock_data/"
     
-#     startNum = int(properties.getSelection(BIZ_PRE_PROCESS)[TARGET_DATA_LOAD_PERIOD])
-    startNum = -3
+    # Write
+    '''
+    startNum = int(properties.getSelection(BIZ_PRE_PROCESS)[TARGET_DATA_LOAD_PERIOD])
     start = getDayFromSpecificDay(time.time(), startNum, "%Y%m%d")
     end = getTodayWithFormatting("%Y%m%d")
     
@@ -43,14 +47,25 @@ if __name__ == '__main__':
     df = df.drop('Date', 1)
     df = df[['Close']]
     df.columns = [stockCd]
+    df = df.convert_objects(convert_numeric=True)
+    df.index = pd.to_datetime(df.index)
+    df = df.tz_localize("UTC")
+    df.sort_index(inplace=True)
+    df.to_csv(path + stockCd + ".csv")
+    '''
+    
+    # Read (DataFrame)
+    df = pd.read_csv(path + stockCd + ".csv", index_col='Date')
     df.index = pd.to_datetime(df.index)
     df = df.tz_localize("UTC")
     print df
-    
     
     algo = TradingAlgorithm(initialize=initialize, handle_data=handle_data)
     results = algo.run(df)
     print results[['starting_cash', 'ending_cash', 'ending_value']].head()
     
-#     plt.plot(results.index, results.portfolio_value)
-#     plt.show()
+    
+    '''
+    plt.plot(results.index, results.portfolio_value)
+    plt.show()
+    '''   
